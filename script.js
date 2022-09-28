2de7a35407d19fd87f7901ca716932c73945b6b0
tannguyen <110894101+buvghhj120902@users.noreply.github.com>
2022-09-28 11:26:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+  });
+}
+
+
+
+/**
+ * search toggle
+ */
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    searchBox.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+// Chuyển ảnh banner
+var arr = [
+  'bn1.png',
+  'bn2.png',
+  'bn3.png',
+  'bn4.png',
+  'bn5.png',
+  
+];
+var i = 0;
+function next() {
+  i++;
+  if (i >= arr.length)
+      i = 0;
+  var anh = document.getElementById('anh');
+  anh.src = arr[i];
+}
+setInterval('next()',5000)
